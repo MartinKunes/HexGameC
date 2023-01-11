95a3b5e8a46921ca35bd7096404c59472ebd0403
MartinKunes <84368626+MartinKunes@users.noreply.github.com>
2023-01-11 23:19:44 +0100

    Done
    Hra funguje.. jenom vizuálně jsou vidět barvy, ale nejde s nima hrát

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var boxs = document.querySelectorAll(".box");
 var popis = document.getElementById("popis");
 var rf = document.querySelector("#rf");
 var resetB = document.querySelector("#reset");
-var modB = document.querySelectorAll(".mode");
+var modB = document.querySelectorAll(".d");
 var hex = ["a","b","c","d","e","f",0,1,2,3,4,5,6,7,8,9];
 
 initiate();
@@ -13,77 +13,6 @@ function initiate(){
     setupModBu();
     setupBox();
     reset();
-}
-
-function reset(){
-    barvicky = generateRandomColor(pocet);
-
-    vyber = pickRandomColor();
-
-    popis.textContent = vyber;
-    resetB.textContent = "Reset";
-
-    for(var i = 0; i < boxs.length; i++){
-        if(barvicky[i]){
-            boxs[i].style.display = "block";
-            boxs[i].style.background = barvicky[i];
-        }else{
-            boxs[i].style.display = "none;"
-        }
-    }
-}
-
-resetB.addEventListener("click", function() {
-    reset();
-})
-
-function setupModBu(){
-    for(var i = 0; i < modB.length; i++){
-        modB[i].addEventListener("click", function(){
-            modB[0].classList.remove("selected");
-            modB[1].classList.remove("selected");
-            modB[2].classList.remove("selected");
-            this.classList.add("selected");
-            if(this.textContent === "Easy"){
-                pocet = 3;
-            } else if(this.textContent === "Medium"){
-                pocet = 6;
-            }else {
-                pocet = 9;
-            }
-            reset();
-        });
-    }
-}
-
-function setupBox(){
-    for(var i = 0; i < boxs.length; i++){
-        boxs[i].addEventListener("click", function () {
-            var rgb = this.style.background;
-            function rgb2hex(rgb){
-                if(rgb.search("rgb") == -1) {
-                    return rgb;
-                }else{
-                    rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
-
-                    function hexx(x){
-                        return("0" + parseInt(x).toString(16)).slice(-2);
-                    }
-                    return"#" + hexx(rgb[1]) + hexx(rgb[2]) + hexx(rgb[3]);
-                }
-            }
-            var klik = rgb2hex(rgb);
-
-            console.log(klik, vyber);
-            if(klik === vyber){
-                rf.textContent = "Correct!!!";
-                resetB.textContent="Play Again??"
-            }else{
-                this.style.background = "#232323"
-                rf.textContent = "Try Again";
-            }
-        });
-    }
 }
 
 function pickRandomColor(){
@@ -110,9 +39,78 @@ function randomColor(){
     return "#" + hex1 + hex2 + hex3 + hex4 + hex5 + hex6;
 }
 
+function reset(){
+    barvicky = generateRandomColor(pocet);
 
+    vyber = pickRandomColor();
 
+    popis.textContent = vyber;
+    resetB.textContent = "Reset";
+    for(var i = 0; i < boxs.length; i++){
+        if(barvicky[i]){
+            boxs[i].style.display = "block";
+            boxs[i].style.background = barvicky[i];
+        }else{
+            boxs[i].style.display = "none;"
+        }
+    }
+}
 
+resetB.addEventListener("click", function() {
+    reset();
+})
+
+function setupModBu(){
+    for(var i = 0; i < modB.length; i++){
+        modB[i].addEventListener("click", function(){
+            modB[0].classList.remove("selected");
+            modB[1].classList.remove("selected");
+            modB[2].classList.remove("selected");
+            this.classList.add("selected");
+            if(this.textContent === "Easy"){
+                reset();
+                pocet = 3;
+            } else if(this.textContent === "Medium"){
+                reset();
+                pocet = 6;
+            }else {
+                reset();
+                pocet = 9;
+            }
+            reset();
+        });
+    }
+}
+
+function setupBox(){
+    for(var i = 0; i < boxs.length; i++){
+        boxs[i].addEventListener("click", function () {
+            var rgb = this.style.background;
+            function rgbhex(rgb){
+                if(rgb.search("rgb") == -1) {
+                    return rgb;
+                }else{
+                    rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+
+                    function hexx(x){
+                        return("0" + parseInt(x).toString(16)).slice(-2);
+                    }
+                    return"#" + hexx(rgb[1]) + hexx(rgb[2]) + hexx(rgb[3]);
+                }
+            }
+            var klik = rgbhex(rgb);
+
+            console.log(klik, vyber);
+            if(klik === vyber){
+                rf.textContent = "Correct!!!";
+                resetB.textContent="Play Again??"
+            }else{
+                this.style.background = "#232323"
+                rf.textContent = "Try Again";
+            }
+        });
+    }
+}
 
 
 
